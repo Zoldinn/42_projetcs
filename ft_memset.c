e53c700b52c073a73c2527415f03a52a4c417f38
lefoffan - leon <lefoffan@student.42perpignan.fr>
2024-11-08 17:53:32 +0100

    I've done a big main test but I'm too tired to finish it well

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: lefoffan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:43:20 by lefoffan          #+#    #+#             */
-/*   Updated: 2024/11/07 16:23:32 by lefoffan         ###   ########.fr       */
+/*   Updated: 2024/11/08 17:48:37 by lefoffan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,70 @@ void	*ft_memset(void *s, int c, size_t n)
 		x[n] = (unsigned char) c;
 	}
 	return (s);
+}
+
+///////////////////
+
+#include <stdio.h>
+#include <string.h> // pour test vrai fonction
+#include <limits.h> // pour tester les limites
+
+void	ft_check(int c, size_t n)
+{
+	int	*tb1;
+	int	*tb2;
+	int	ok;
+
+	tb1 = ft_calloc(n, sizeof(int));
+	tb2 = ft_calloc(n, sizeof(int));
+	ft_memset(tb1, c, n * sizeof(int));
+	ft_memset(tb2, c, n * sizeof(int));
+	ok = 1;
+	while (n)
+	{
+		n--;
+		if (tb1[n] != tb2[n])
+		{
+			printf("/!\\n> ft_memset : %d\n>memset : %d\n\n", tb1[n], tb2[n]);
+			ok = 0;
+			break;
+		}
+		printf("ft_memset : %d, memset : %d.\n", tb1[n], tb2[n]);
+	}
+	if (ok)
+		printf("Everything : OK !\n");
+	free(tb1);
+	free(tb2);
+	printf("\n----------------------------------\n");
+}
+
+void	ft_repeat(int *tb, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+		printf("%d, ", tb[i++]);
+	printf("\n");
+}
+
+int	main(void)
+{
+	// test pour n = 0;
+	int	tb0[5] = {5, 5, 5, 5, 5};
+
+	printf("TEST : size = 0\norigin : tab = ");
+	ft_repeat(tb0, 5);
+	ft_memset(tb0, 0, 3 * sizeof(int));
+	printf("next : tab = ");
+	ft_repeat(tb0, 5);
+	printf("\n------------------------------------------------------------\n");
+	// test limites
+	ft_check(0, 3);
+	ft_check(INT_MAX, 5);
+	ft_check(INT_MIN, 5);
+	printf("\n------------------------------------------------------------\n");
+	// test normal
+	ft_check(50, 10);
+	return (0);
 }
